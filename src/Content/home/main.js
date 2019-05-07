c7e6ab4b7ce328a08b86685e90860f2c201f4d6b
Corey Dunkin <corey.dunkin@gmail.com>
2019-05-08 09:24:28 +1000

    added parallax

--- src/Content/home/main.js
+++ src/Content/home/main.js
@@ -4,21 +4,32 @@ import ReactFullPage, {fullpageApi} from '@fullpage/react-fullpage';
 
 import Content, {MySection} from '../main';
 
+import Parallax from 'parallax-js';
+
 export const newContent = Content;
 
 class Home extends Component {
   // This syntax ensures `this` is bound within handleClick.
   // Warning: this is *experimental* syntax.
   
+  componentDidMount() {
+    this.Parallax = new Parallax(this.scene);
+    this.Parallax.friction(0.2, 0.2);
+  }
+
+  componentWillUnmount() {
+    this.Parallax.disable();
+  }
+
   
   render() {
     
-    
+
     console.log(Content.prototype);
 
     return (
       <div className="home page">
-      
+          
           <h1 className="hero">
       <Animated 
         animationIn="fadeIn"
@@ -40,19 +51,22 @@ class Home extends Component {
       <Animated
         animationIn="fadeInDown"
         animationOut="fadeOutUp"
-        animationInDelay={5000}
+        animationInDelay={4500}
         animationInDuration={900}
         isVisible={true}>
 
           <h2 className="subhero">
-          <span>&#8643;</span><a href="#About">NEXT PAGE</a><span>&#8642;</span>
+            <a href="#About">NEXT PAGE</a>
           </h2>
 
 
       </Animated>    
 
-         
-      
+        <div className="scene"  ref={el => this.scene = el}>
+          <h3 data-depth="0.4">THIS IS SOME PARALLAX STUFF</h3>
+          <p data-depth="0.2">More parallax stuff.</p>
+          <p data-depth="0.0">This too.</p>
+        </div>
       </div>
     );
   }
